d84016e34b72a388f3af01539d29b343f3fe0857
Andrés <andrewvergel@gmail.com>
2014-04-17 01:52:09 -0300

    Fixed #15

---
+++ app/Model/cake_sessions.php
@@ -0,0 +1,7 @@
+<?php 
+class cake_sessions extends AppModel{
+       public function beforeSave($options = array()){
+       // Useful in cases where you would want to store extra data - like an IP-address - in your session handler
+    }
+}
+?>
